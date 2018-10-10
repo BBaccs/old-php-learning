d6e61dbf815fa12959f60f6786f5a0682edd36ca
Brian Baccarella <baccarellab@gmail.com>
2018-10-10 11:52:29 -0400

    Update another_index.php

---
+++ another_index.php
@@ -0,0 +1,14 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=asdfas', 'root', '');
+} catch (PDOException $e) {
+    die('Could not connect.');
+}
+
+
+$statement
+
+
+
+require 'index.view.php';
